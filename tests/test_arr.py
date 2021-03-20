d6350509e1a14057bd5e9595406c93fe121109c6
Hamitcan Malkoç <hamitcanmalkoc@gmail.com>
2021-03-20 23:32:47 +0300

    adding arr module test

--- tests/test_arr.py
+++ tests/test_arr.py
@@ -2,7 +2,28 @@ import pytest
 
 
 def test_arr():
-    pass
+    import numpy as np
+    from hz.arr import functional
+
+    arr1 = [
+        [[1., 2., 3.], [4., 5., 6.]],
+        [[7., 8., 9.], [10., 11., 12.]]
+    ]
+    arr2 = [[2., 3., 4.], [5., 6., 7.]]
+
+    print(np.std(np.asarray(arr1)))
+    print(np.std(np.asarray(arr1), axis=0))
+    print(np.std(np.asarray(arr1), axis=1))
+    print(np.std(np.asarray(arr1), axis=2))
+    print()
+    print(functional.std(functional.asarray(arr1)))
+    print(functional.std(functional.asarray(arr1), axis=0))
+    print(functional.std(functional.asarray(arr1), axis=1))
+    print(functional.std(functional.asarray(arr1), axis=2))
+    print()
+
+    print(np.asarray(arr1) - np.asarray(arr2))
+    print(functional.asarray(arr1) - functional.asarray(arr2))
 
 
 if __name__ == '__main__':
