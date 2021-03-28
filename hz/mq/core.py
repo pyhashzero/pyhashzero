436ce8dc40c4b2fa65226e5d47cf928a832d0591
Hamitcan Malkoç <hamitcanmalkoc@gmail.com>
2021-03-28 12:51:50 +0300

    removing cache, log and mq modules

--- hz/mq/core.py
+++
@@ -1,9 +0,0 @@
-from hz.core import CoreObject
-
-
-class MQ(CoreObject):
-    def __init__(self):
-        super(MQ, self).__init__()
-
-    def send(self, message):
-        raise NotImplementedError
